6d38eed442c53f07aed3a89960ca7cfc46b691e8
mangenotwork <2912882908@qq.com>
2019-09-24 15:21:07 +0800

    0.1.4
    0.1.4

--- example.py
+++ example.py
@@ -1,6 +1,19 @@
 import manms
 
 #获取身份证地址
-print(manms.getsfzadd("51300*********36"))
+print(manms.getsfzadd("513001199506200836"))
+#获取身份证性别
+print(manms.getsfzsex("513001199506200836"))
+#检查身法有效性
+print(manms.sfzcheck("513001199506200836"))
+#分词功能
+print(manms.fenci("你好世界"))
+#找关键词
+print(manms.gjc("你好世界"))
+#英汉翻译
+print(manms.yhfy("你好世界"))
+#汉语转拼音
+print(manms.piying("你好世界"))
 
-
+	
+	
--- manms/__init__.py
+++ manms/__init__.py
@@ -4,8 +4,31 @@ __date__    = "20190923"
 
 from manms import method
 
-def getsfzadd(arg):
+def getsfzadd(sfznumber):
 	#获取身份证地址
-	return method.ManMS_Autoc._sfzverify_add(arg)
+	return method.ManMS_Autoc._sfzverify_add(sfznumber)
 
+def getsfzsex(sfznumber):
+	#获取身份证性别
+	return method.ManMS_Autoc._sfzverify_sex(sfznumber)
+
+def sfzcheck(sfznumber):
+	#验证身份证有效性
+	return method.ManMS_Autoc._sfzcheck(sfznumber)
+
+def fenci(string):
+	#分词功能
+	return method.ManMS_Lang._fenci(string)
+
+def gjc(string):
+	#找关键词
+	return method.ManMS_Lang._gjc(string)
+
+def yhfy(string):
+	#英汉翻译
+	return method.ManMS_Lang._yhfanyi(string)
+
+def piying(string,types="0"):
+	#汉语转拼音
+	return method.ManMS_Lang._pingyin(string,types)
 
